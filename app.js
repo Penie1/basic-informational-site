bfbab4c3ac379442d27e11046453cdb9187a88fd
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-11-28 11:38:31 +0300

    Refactor to use Express

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-import http from "http";
-import fs from "node:fs";
-const app = http.createServer((req, res) => {
-  const url = req.url;
-  res.writeHead(200, { "Content-Type": "text/html" });
-  let path = "./view/";
-  switch (url) {
-    case "/":
-      path += "index.html";
-      break;
+import express from "express";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+const port = process.env.PORT || 3000;
 
-    case "/about.html":
-      path += "about.html";
-      break;
-
-    case "/contact.html":
-      path += "contact-me.html";
-      break;
-
-    default:
-      path += "404.html";
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      throw new Error(err);
-    } else {
-      res.end(data);
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
 });
-
-app.listen(8000);
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/view/about.html");
+});
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/view/contact-me.html");
+});
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/view/404.html");
+});
+app.listen(port, () => {
+  console.log(`Listening on port ${port || 3000}!`);
+});
